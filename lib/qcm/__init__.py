b700572dd41e5ff1ee2f5b5a6c217526ffded081
adorsk <adorsk>
2013-03-18 13:32:52 -0400

    more work on model

--- lib/qcm/__init__.py
+++ lib/qcm/__init__.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import json
 
 
 class QuotaChangeModelRunner(object):
@@ -8,63 +9,72 @@ class QuotaChangeModelRunner(object):
         self.acl = {}
 
         self.ingest_inputs()
-
-        # process trips to throw out trips w/ low gfish rats.
+        self.process_trips()
 
     def ingest_inputs(self):
-        # catch.
+        self.ingest_catch()
+        self.ingest_cost()
+        self.ingest_acl()
+
+    def ingest_catch(self):
         #"MULT_YEAR","mri","TRIP_ID","spec","stock","stock_id1","_TYPE_","_FREQ_","land","disc"
         #"2010","2192","2010_100409_0001","non_gfish","all","23","0","1","0","."
+        # Read catch records from file, sum on stock and
+        # gfish/non_gfish spec.
         catch_file = "/home/adorsk/projects/noaa/qcm/test_data/catch.csv"
         with open(catch_file, "rb") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 trip_id = row.get('TRIP_ID')
+                
+                # Ignore records lacking id or spec.
                 if trip_id is None: continue
+                if row.get('spec') is None: continue
+
+                # Get landed/discard (default=0 for empty or '.')
+                for field in ['land', 'disc']:
+                    try: row[field] = float(row[field])
+                    except: row[field] = 0
+
+                # Ignore records with no catch.
+                catch = row['land'] + row['disc']
+                if not catch: continue
 
                 # Get or initialize trip.
                 trip = self.trips.setdefault(trip_id, {
                     'mri': row['mri'],
                     'stock_catches': {},
-                    'spec_catches': {},
+                    'spec_totals': {},
                 })
 
-                # Ignore trips that don't have 'spec.'
-                if row.get('spec') is None: continue
-
-                # Get landed/discard (default=0 for empty or '.')
-                for field in ['land', 'disc']:
-                    try: row[field] = float(row['field'])
-                    except: row[field] = 0
-
-                # Calculate catch and ignore records with no catch.
-                catch = row['land'] + row['disc']
-                if not catch: continue
-
-                # Add catch to stock catches
+                # Add catch to stock catch lists.
                 stock_id = row['stock_id1']
-                stock_catches = trip['stock_catches']
-                stock_catches[stock_id] = stock_catches.setdefault(
-                    stock_id, 0) + catch
+                trip['stock_catches'].setdefault(stock_id, []).append(catch)
 
-                # Add to spec catches.
+                # Add to spec catch totals.
                 spec = row['spec']
                 if spec != 'non_gfish':
                     spec = 'gfish'
-                spec_catches[spec] = spec_catches.setdefault(spec, 0) + catch
+                trip['spec_totals'][spec] = trip['spec_totals'].setdefault(
+                    spec, 0) + catch
 
+    def ingest_cost(self):
         # costs.
         #"TRIP_ID","trip_revenue","trip_cost","_TYPE_","_FREQ_","quota_cost","sector_cost","variable_cost"
         #"2010_100409_0001","0",".","0","1","0","0","."
         cost_file = "/home/adorsk/projects/noaa/qcm/test_data/costs.csv"
-        with open(catch_file, "rb") as f:
+        with open(cost_file, "rb") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 trip_id = row.get('TRIP_ID')
-                if trip_id is None: continue
-                trip = self.trips.setdefault(trip_id, {})
-                trip.update(row)
+                trip = self.trips.get(trip_id)
+                if not trip:
+                    continue
+                for field in ['trip_revenue', 'variable_cost']:
+                    try: trip[field] = float(row[field])
+                    except: trip[field] = 0
 
+    def ingest_acl(self):
         # acl.
         #"spec","stock","stock_id1","limit_1","limit_2","limit_3"
         #"am_plaice","all","7","6058240.8","0","0"
@@ -74,10 +84,37 @@ class QuotaChangeModelRunner(object):
             for row in reader:
                 stock_id = row.get('stock_id1')
                 if stock_id is None: continue
-                stock = self.acl.setdefault(trip_id, {})
+                stock = self.acl.setdefault(stock_id, {})
                 stock.update(row)
 
+    def process_trips(self):
+        for trip_id, trip in self.trips.items():
+            # Filter out trips that have low gfish catch, 
+            # or low groundfish ratios.
+            spec_totals = trip['spec_totals']
+            gfish = spec_totals.get('gfish', 0)
+            non_gfish = spec_totals.get('non_gfish', .1)
+            if gfish < 15 or 1.0 * gfish/non_gfish <= .0075:
+                del self.trips[trip_id]
+                continue
+
+            # Calculate net revenue.
+            trip['netrev'] = trip['trip_revenue'] - trip['variable_cost']
+
+            # Calculate ACE efficiency per stock.
+            # Note: each catch list in stock_catches should normally
+            # just have one item, but in case there are multiple
+            # items, the mean of the valus is used, as per chad's
+            # SAS code.
+            trip['stock_efficiencies'] = {}
+            for stock_id, catches in trip['stock_catches'].items():
+                mean_catch = float(sum(catches))/len(catches)
+                if mean_catch > 0:
+                    ace_effic = trip['netrev']/mean_catch
+                    trip['stock_efficiencies'][stock_id] = ace_effic
+
     def calculate_p_scores(self):
+        """ Calculate probability scores for each trip."""
         pass
 
     def run_simulations(self):
